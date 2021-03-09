86ccd4e4716cf04082f6c1738dad12ffea67b08c
Jabr0ski <51535527+Jabr0ski@users.noreply.github.com>
2021-03-09 20:15:07 +0800

    Added Players and Card Dealing

--- CockroachPoker.py
+++ CockroachPoker.py
@@ -7,13 +7,15 @@ class Card:
         #self.uid = uid
 
 class Player:
-    def __init__(self, uid, hand, collection):
+    def __init__(self, uid, hand, collection, viewed):
         self.uid = uid
-        self.name = hand
-        self.value = collection
+        self.hand = hand
+        self.collection = collection
+        self.viewed = viewed
 
 Suits = ["Cockroach", "Stink Bug", "Spider", "Scorpion", "Fly", "Bat", "Rat", "Toad"]
 Deck = []
+Players = []
 
 def buildDeck():
     for s in Suits:
@@ -21,11 +23,28 @@ def buildDeck():
             c = Card(s, i)
             Deck.append(c)
     shuffle()
-    #for c in Deck:
-        #print(c.name, c.value)
-        
+
+#shuffle the deck
 def shuffle():
     random.shuffle(Deck)
+
+#deal cards out to participating player and AIs
+def deal():
+    firstPlayerIndex = random.randint(1, len(Players))
+    currPlayerIndex = firstPlayerIndex
+    while len(Deck) > 0:
+        currPlayer = findPlayer(currPlayerIndex % len(Players)+1)
+        currPlayer.hand.append(Deck.pop())
+        currPlayerIndex += 1
+    firstPlayerIndex = (firstPlayerIndex % len(Players)+1)
+    print("\nPlayer", firstPlayerIndex, "starts!")
+    
+#pid = player ID passed to find
+def findPlayer(pid):
+    for p in Players:
+        if(p.uid == pid):
+            #print(p.uid)
+            return p
 
 def main():
     print("Cockroach Poker is a reverse set collection game that has nothing\
@@ -33,16 +52,24 @@ to do with poker – except that the game is all about bluffing, with cards that
 show cockroaches, rats and stink bugs. The goal is to force another player to\
 collect 4 of any one type of critter, or to empty their hand.")
     buildDeck()
-    playerCount = 0
-    while(playerCount < 1 or playerCount > 5):
+    opponentCount = 0
+    while(opponentCount < 1 or opponentCount > 5):
         try:
-            playerCount = int(input('\nHow many AI opponents? Enter a number from 1 to 5: '))
+            opponentCount = int(input('\nHow many AI opponents? Enter a number from 1 to 5: '))
         except:
             print("\nYou must enter a number.")
             continue
-        if(playerCount < 1):
+        if(opponentCount < 1):
             print("\nYou'll have no opponents! Enter a number from 1 to 5.")
-        if(playerCount > 5):
+        if(opponentCount > 5):
             print("\nYou can only have up to 5 opponents. Enter a number from 1 to 5.")
+    
+    for i in range (1, opponentCount + 2):
+        p = Player(i, [], [], False)
+        Players.append(p)
+    
+    print("\nYou are player 1 and you have", opponentCount, "opponents.")
+    
+    deal()
     
 main()
